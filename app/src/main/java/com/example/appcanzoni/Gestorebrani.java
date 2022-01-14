5d902169ebfa4a5e927510022d6a9c6491188806
FrancescoBasso <frnacescobasso03@gmail.com>
2022-01-14 14:11:41 +0100

    AppCanzoni PT.1

---
+++ app/src/main/java/com/example/appcanzoni/Gestorebrani.java
@@ -0,0 +1,18 @@
+package com.example.appcanzoni;
+import java.util.ArrayList;
+
+public class Gestorebrani {
+
+
+    public class Gestorebrani {
+        ArrayList<Brano> ListaBrani;
+
+        public Gestorebrani() {
+            ListaBrani = new ArrayList<Brano>();
+        }
+
+        public void addBrano() {
+
+        }
+    }
+}
