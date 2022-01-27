7467db469f545a3602c509a47d4fa8ab1ce3fb38
FrancescoBasso <frnacescobasso03@gmail.com>
2022-01-27 08:51:02 +0100

    AppCanzoni Log.d

--- app/src/main/java/com/example/appcanzoni/MainActivity.java
+++ app/src/main/java/com/example/appcanzoni/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -21,11 +22,15 @@ public class MainActivity extends AppCompatActivity {
     Spinner spnGeneri;
     String[] generi = {"Pop", "Rock", "Dance", "Rap"};
     Gestorebrani gb;
+    String tag = "Mainactivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int i = 0;
+        i++;
         setContentView(R.layout.activity_main);
+        Log.d(tag, "Lanciato il metodo Oncreate " + i);
 
         btnInserisci = (Button) findViewById(R.id.btnInserisci);
         btnApri = (Button) findViewById(R.id.btnApri);
@@ -34,33 +39,44 @@ public class MainActivity extends AppCompatActivity {
         txtDurata1 = (EditText) findViewById(R.id.txtDurata1);
         txtDatacreazione1 = (EditText) findViewById(R.id.txtDatacreazione1);
         spnGeneri = (Spinner) findViewById(R.id.spnGeneri);
-        ArrayAdapter<String> aaG = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,generi);
+        ArrayAdapter<String> aaG = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, generi);
         spnGeneri.setAdapter(aaG);
         gb = new Gestorebrani();
         btnInserisci.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 String genSelez = spnGeneri.getSelectedItem().toString();
-                gb.addBrano(txtTitolo1.getText().toString(),txtAutore1.getText().toString(),Integer.parseInt(txtDurata1.getText().toString()),
-                        txtDatacreazione1.getText().toString(),genSelez);
+                gb.addBrano(txtTitolo1.getText().toString(), txtAutore1.getText().toString(), Integer.parseInt(txtDurata1.getText().toString()),
+                        txtDatacreazione1.getText().toString(), genSelez);
 
             }
         });
+
 
         btnApri.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 StringBuilder string_fin = new StringBuilder();
                 string_fin = gb.ListaBrani();
-                Intent i = new Intent(getApplicationContext(),MainActivity2.class);
-                i.putExtra("string_fin",string_fin.toString());
+                Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+                i.putExtra("string_fin", string_fin.toString());
                 startActivity(i);
             }
         });
 
-    }
-
-
 
     }
+    @Override
+    protected void onStart() {
+        Log.d(tag, "Sono nel metodo OnStart");
+        super.onStart();
+    }
+    protected void onStop() {
+        Log.d(tag, "Sono nel metodo OnStop");
+        super.onStop();
+
+    }
+}
+
+
+
